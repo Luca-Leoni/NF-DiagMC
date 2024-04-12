5f26bda02e2bf5c4a0642a397629eb3a850edc84
Luca Leoni <luca.leoni.99@gmail.com>
2024-04-12 13:30:58 +0200

    Dump
    Dump of all the files related to the project "Global sampling of Feynman diagrams through Normalizing Flow".
    The code has been refactored to make it suitable for pubblication, but has not been retested right now since needs a cuda machine to compile the ML part.
    Right now the CPU part realted to the DMC library compiles fine, in the next days tests will be performed on the NF module.

---
+++ build/src/CMakeFiles/DMC.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/utente/CODE/NF-DiagMC/src/Axis.cpp" "src/CMakeFiles/DMC.dir/Axis.cpp.o" "gcc" "src/CMakeFiles/DMC.dir/Axis.cpp.o.d"
+  "/home/utente/CODE/NF-DiagMC/src/Bijection.cpp" "src/CMakeFiles/DMC.dir/Bijection.cpp.o" "gcc" "src/CMakeFiles/DMC.dir/Bijection.cpp.o.d"
+  "/home/utente/CODE/NF-DiagMC/src/CDFInverse.cpp" "src/CMakeFiles/DMC.dir/CDFInverse.cpp.o" "gcc" "src/CMakeFiles/DMC.dir/CDFInverse.cpp.o.d"
+  "/home/utente/CODE/NF-DiagMC/src/Histogram.cpp" "src/CMakeFiles/DMC.dir/Histogram.cpp.o" "gcc" "src/CMakeFiles/DMC.dir/Histogram.cpp.o.d"
+  "/home/utente/CODE/NF-DiagMC/src/LinearFunApprox.cpp" "src/CMakeFiles/DMC.dir/LinearFunApprox.cpp.o" "gcc" "src/CMakeFiles/DMC.dir/LinearFunApprox.cpp.o.d"
+  "/home/utente/CODE/NF-DiagMC/src/NewtonFunApprox.cpp" "src/CMakeFiles/DMC.dir/NewtonFunApprox.cpp.o" "gcc" "src/CMakeFiles/DMC.dir/NewtonFunApprox.cpp.o.d"
+  "/home/utente/CODE/NF-DiagMC/src/SimOptions.cpp" "src/CMakeFiles/DMC.dir/SimOptions.cpp.o" "gcc" "src/CMakeFiles/DMC.dir/SimOptions.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/DMC.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DMC.dir/Axis.cpp.o"
+  "CMakeFiles/DMC.dir/Axis.cpp.o.d"
+  "CMakeFiles/DMC.dir/Bijection.cpp.o"
+  "CMakeFiles/DMC.dir/Bijection.cpp.o.d"
+  "CMakeFiles/DMC.dir/CDFInverse.cpp.o"
+  "CMakeFiles/DMC.dir/CDFInverse.cpp.o.d"
+  "CMakeFiles/DMC.dir/Histogram.cpp.o"
+  "CMakeFiles/DMC.dir/Histogram.cpp.o.d"
+  "CMakeFiles/DMC.dir/LinearFunApprox.cpp.o"
+  "CMakeFiles/DMC.dir/LinearFunApprox.cpp.o.d"
+  "CMakeFiles/DMC.dir/NewtonFunApprox.cpp.o"
+  "CMakeFiles/DMC.dir/NewtonFunApprox.cpp.o.d"
+  "CMakeFiles/DMC.dir/SimOptions.cpp.o"
+  "CMakeFiles/DMC.dir/SimOptions.cpp.o.d"
+  "libDMC.pdb"
+  "libDMC.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DMC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
